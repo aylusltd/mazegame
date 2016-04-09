fbfe7df4fb3f1d08add5f84d55d2cae555adabc8
Mike Stoller <mikestolleronline@gmail.com>
2016-04-09 11:59:45 -0400

    added jade task to gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var webpack = require('gulp-webpack');
+var jade = require('gulp-jade');
 
-
+var jsInput = './src/app/**'
+var viewsInput = './src/views/**/*.jade';
 var scssInput = './src/scss/**/*.scss';
 var output = './dist';
 
@@ -17,16 +19,31 @@ gulp.task('sass', function () {
 		.pipe(gulp.dest(output));
 });
 
+gulp.task('scripts', function() {
+  return gulp.src(jsInput)
+    // .pipe(webpack())
+    .pipe(gulp.dest(output));
+});
+
+gulp.task('views', function() {
+  // var YOUR_LOCALS = {};
+  gulp.src(viewsInput)
+    .pipe(jade())
+    .pipe(gulp.dest('./dist/'));
+});
+
 gulp.task('watch', function() {
   return gulp
     // Watch the input folder for change,
     // and run `sass` task when something happens
-    .watch(scssInput, ['sass'])
+    gulp.watch(scssInput, ['sass']);
+    gulp.watch(jsInput, ['scripts']);
+    gulp.watch(viewsInput, ['views']);
     // When there is a change,
     // log a message in the console
-    .on('change', function(event) {
+    gulp.on('change', function(event) {
       console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
     });
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['sass', 'scripts', 'views', 'watch']);
