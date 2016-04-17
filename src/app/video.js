59939b6a3e3f4678136e6673104579dec44e393f
Jason Nichols <github@ayl.us>
2016-04-17 00:49:56 -0400

    heroku

--- src/app/video.js
+++ src/app/video.js
@@ -1,9 +1,9 @@
 
 module.exports = function videoHandler(ev){
-	var rtc = require('./webrtc.client');
+	// var rtc = require('./webrtc.client');
 	
 	var t = ev.target;
-	var socket = window.io('https://localhost:443');
+	// var socket = window.io('https://localhost:443');
 
 	while(t !== document.body && !~Array.prototype.slice.call(t.classList).indexOf('chair')) {
 		t=t.parentNode;
@@ -13,12 +13,12 @@ module.exports = function videoHandler(ev){
 		var selfie = t.querySelector('video');
 		selfie.setAttribute('muted', true);
 		selfie.src = window.URL.createObjectURL(stream);
-		rtc.createStream({"video": true, "audio":false}, function(stream){
+		// rtc.createStream({"video": true, "audio":false}, function(stream){
 		    // get local stream for manipulation
 		    // rtc.attachStream(stream, 'local');
-		});
-		rtc.connect('wss://localhost:8001');
-		socket.emit('video', {chair: t.getAttribute('data-chair')});
+		// });
+		// rtc.connect('wss://localhost:8001');
+		// socket.emit('video', {chair: t.getAttribute('data-chair')});
 	}
 
 	function videoError(error){
@@ -29,11 +29,11 @@ module.exports = function videoHandler(ev){
 	if (navigator.getUserMedia) {       
         navigator.getUserMedia({video: true, audio:true}, handleVideo, videoError);
     }
-    socket.on('video', function(message){
-    	rtc.on('add remote stream', function(stream){
-			// show the remote video
-			document.querySelector('[data-chair='+message.chair+'] video').src=URL.createObjectURL(stream);
-  		});
-    });
+    // socket.on('video', function(message){
+   //  	rtc.on('add remote stream', function(stream){
+			// // show the remote video
+			// document.querySelector('[data-chair='+message.chair+'] video').src=URL.createObjectURL(stream);
+  	// 	});
+    // });
 	
 };
